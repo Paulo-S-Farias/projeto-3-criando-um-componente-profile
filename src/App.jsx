982754791159b427d80eb4757bc3be5f401cb7c6
Paulo s. Farias <159643226+Paulo-S-Farias@users.noreply.github.com>
2024-11-06 16:21:03 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Profile from "./components/Profile"
+
+export default function App(){
+  return(
+    <body>
+      <Profile img={'./public/Mft.jpg'} name={'Paulo Eduardo'} bio={'𝚃𝚎𝚌.𝚍𝚎𝚜𝚎𝚗𝚟𝚘𝚕𝚟𝚒𝚖𝚎𝚗𝚝𝚘 𝚍𝚎 𝚜𝚒𝚜𝚝𝚎𝚖𝚊 2° ano.'} phone={'+ 55 (85) 92444813'} email={'paulofarias34@gmail.com'} githubUrl={'https://github.com/Paulo-S-Farias'} InstagramUrl={'https://www.instagram.com/paulofarias9793'}></Profile>
+    </body> 
+  )
+}
