68d39b5c2fb18ae9ebaffebba56371d2c5f7b5e0
Paulo s. Farias <159643226+Paulo-S-Farias@users.noreply.github.com>
2024-11-06 21:34:15 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Profile from "./components/Profile"
 export default function App(){
   return(
     <body>
-      <Profile img={'./public/Mft.jpg'} name={'Paulo Eduardo'} bio={'𝚃𝚎𝚌.𝚍𝚎𝚜𝚎𝚗𝚟𝚘𝚕𝚟𝚒𝚖𝚎𝚗𝚝𝚘 𝚍𝚎 𝚜𝚒𝚜𝚝𝚎𝚖𝚊 2° ano.'} phone={'+ 55 (85) 92444813'} email={'paulofarias34@gmail.com'} githubUrl={'https://github.com/Paulo-S-Farias'} InstagramUrl={'https://www.instagram.com/paulofarias9793'}></Profile>
+      <Profile img={'./Mft.jpg'} name={'Paulo Eduardo'} bio={'𝚃𝚎𝚌.𝚍𝚎𝚜𝚎𝚗𝚟𝚘𝚕𝚟𝚒𝚖𝚎𝚗𝚝𝚘 𝚍𝚎 𝚜𝚒𝚜𝚝𝚎𝚖𝚊 2° ano.'} phone={'+ 55 (85) 92444813'} email={'paulofarias34@gmail.com'} githubUrl={'https://github.com/Paulo-S-Farias'} InstagramUrl={'https://www.instagram.com/paulofarias9793'}></Profile>
     </body> 
   )
 }
